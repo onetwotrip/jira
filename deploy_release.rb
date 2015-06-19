608ac66024bd91a863733fa3d1880890f0912cf2
Sergey Melnik <admin.sa@gmail.com>
2015-06-19 18:46:10 +0300

    get repos and branches

--- deploy_release.rb
+++ deploy_release.rb
@@ -1,6 +1,7 @@
 require 'jira'
 require 'slop'
 require 'pp'
+require 'java-properties'
 require './lib/issue'
 
 opts = Slop.parse do |o|
@@ -8,7 +9,7 @@ opts = Slop.parse do |o|
   o.string '-u', '--username', 'username', default: 'default'
   o.string '-p', '--password', 'password', default: 'bWx3h6wjHgHEyi'
   o.string '--site', 'site', default: 'default'
-  o.string '--contextpath', 'context path', default: ''
+  o.string '--context_path', 'context path', default: ''
   o.string '--release', 'release', default: 'OTT-4487'
 
   o.on '--help', 'print the help' do
@@ -17,14 +18,20 @@ opts = Slop.parse do |o|
   end
 end
 
+env_export = {}
+
 options = { auth_type: :basic }.merge(opts.to_hash)
 client = JIRA::Client.new(options)
 
-issue = client.Issue.find(opts[:release])
-issue.opts_setter opts
-issue.related['branches'].each do |branch|
+release = client.Issue.find(opts[:release])
+
+release.related['branches'].each do |branch|
   puts branch['name']
   puts branch['repository']['name']
-  ENV["#{branch['repository']['name'].upcase}_DEPLOY"] = 'true'
-  ENV["#{branch['repository']['name'].upcase}_BRANCH"] = branch['name']
+  env_export["#{branch['repository']['name'].upcase}_DEPLOY"] = 'true'
+  env_export["#{branch['repository']['name'].upcase}_BRANCH"] = branch['name']
 end
+
+pp env_export
+
+JavaProperties.write(env_export, './.properties')
