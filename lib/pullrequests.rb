6d6f878e6266f0ffa1dcbc3795678d90c4f4c01d
Dmitry Shmelev <avikez@gmail.com>
2016-02-05 14:16:28 +0300

    SRV-735: Add debug info of fails to issue comment

--- lib/pullrequests.rb
+++ lib/pullrequests.rb
@@ -2,8 +2,17 @@ module JIRA
   ##
   # This class represents an array of PullRequests
   class PullRequests < Array
+    attr_reader :valid_msg
+
     def valid?
       !empty? && items_valid? && !duplicates?
+    rescue => e
+      @valid_msg = e
+      return false
+    end
+
+    def empty?
+      fail 'Has no PullRequests' if super
     end
 
     private
@@ -11,15 +20,14 @@ module JIRA
     def items_valid?
       each do |item|
         if (!item.is_a? Hash) || (!item.dig('source', 'url'))
-          puts "Item of PullRequests is not valid: #{item.inspect}"
-          return false
+          fail "Item of PullRequests is not valid: #{item.inspect}"
         end
       end
     end
 
     def duplicates?
       urls = map { |i| i['source']['url'] }
-      urls.uniq.length != urls.length
+      fail "PullRequests has duplication: #{urls.join ','}" if urls.uniq.length != urls.length
     end
   end
 end
