f62852e8dc53254812fbcd0fe5425a991ba3bedb
Dmitry Shmelev <avikez@gmail.com>
2016-06-14 13:30:46 +0300

    CodeReview: move bincode to libs scenarios

---
+++ lib/scenarios.rb
@@ -0,0 +1,21 @@
+# Global requirements
+require 'jira'
+require 'issue'
+require 'ottinfra/sendmail'
+require 'simple_config'
+require 'colorize'
+require 'json'
+require 'git'
+require 'rest-client'
+require 'addressable/uri'
+
+require 'check'
+require 'repo'
+require 'issue'
+
+# Scenarios
+require 'scenarios/codereview'
+require 'scenarios/reviewrelease'
+require 'scenarios/buildrelease'
+require 'scenarios/freezerelease'
+require 'scenarios/postactionrelease'
