c205c81f37c6db5f136d433d7737504ed8376249
sergey <sergey.matveev@onetwotrip.com>
2018-04-11 12:42:36 +0300

    Add logger to issue + build error checks

--- lib/scenarios/checkconflicts.rb
+++ lib/scenarios/checkconflicts.rb
@@ -6,24 +6,39 @@ module Scenarios
       issue.jql("filter=#{filter}")
     rescue JIRA::HTTPError => jira_error
       error_message = jira_error.response['body_exists'] ? jira_error.message : jira_error.response.body
-      puts "Error in JIRA with the search by filter #{error_message}".red
+      LOGGER.info "Error in JIRA with the search by filter #{error_message}".red
       []
     end
 
     def check_issue(issue_task)
+      LOGGER.info "Start check #{issue_task.key} issue".green
       issue_task.api_pullrequests.each do |pr|
-        diff_in_pr = pr.diff
-        commit_id = pr.source['commit']['hash']
-        commit = BITBUCKET.repo(pr.repo_owner, pr.repo_slug).commit(commit_id)
-        status_of_build = commit.build_statuses.collect.last
-        conflict_flag = diff_in_pr.include? '<<<<<<<'
-        log_string = "Status of pullrequest #{pr.title} is #{status_of_build.name}:#{status_of_build.state} and ".green
-        conflict_flag_log = "conflict_flag is #{conflict_flag}".green
-        conflict_flag_log = "conflict_flag is #{conflict_flag}".red if conflict_flag
+        begin
+          diff_in_pr        = pr.diff
+          commit_id         = pr.source['commit']['hash']
+          commit            = BITBUCKET.repo(pr.repo_owner, pr.repo_slug).commit(commit_id)
+          status_of_build   = commit.build_statuses.collect.last
+          if status_of_build.state.upcase.include? 'FAILED'
+            LOGGER.info "Detected build status error in #{issue_task.key}. Writing comment in ticket...".red
+            issue_task.post_comment 'Ticket has build error status, pls check it'
+          end
+          conflict_flag     = diff_in_pr.include? '<<<<<<<'
+          log_string        = "Status of pullrequest #{pr.title} is #{status_of_build.name}:#{status_of_build.state} and ".green
+          conflict_flag_log = "conflict_flag is #{conflict_flag}".green
+          if conflict_flag
+            LOGGER.info "Find conflicts in #{issue_task.key}. Writing comment in ticket..."
+            conflict_flag_log = "conflict_flag is #{conflict_flag}".red
+            issue_task.post_comment 'After last release this issue started to have merge conflicts. Please fix it'
+            LOGGER.info "Finished writing merge conflict message in #{issue_task.key}"
+          end
 
-        log_string += conflict_flag_log + " with link https://bitbucket.org/OneTwoTrip/#{pr.repo_slug}/".green +
-                      "pull-requests/#{pr.id}".green
-        puts log_string
+          log_string += conflict_flag_log + " with link https://bitbucket.org/OneTwoTrip/#{pr.repo_slug}/".green +
+            "pull-requests/#{pr.id}".green
+          LOGGER.info log_string
+
+        rescue StandardError => error
+          LOGGER.info "There is error occured with ticket #{issue_task.key}: #{error.message}".red
+        end
       end
     end
 
@@ -32,16 +47,28 @@ module Scenarios
       filter = SimpleConfig.filter
 
       unless filter
-        puts 'No necessary params - filter'.red
+        LOGGER.info 'No necessary params - filter'.red
         exit
       end
 
-      puts "Check conflicts in tasks from filter #{filter}".green
+      LOGGER.info "Check conflicts in tasks from filter #{filter}".green
       client = JIRA::Client.new SimpleConfig.jira.to_h
       issues = filter && find_by_filter(client.Issue, filter)
-      puts 'Start check issues'.green
+      LOGGER.info 'Start check issues'.green
       issues.each { |issue| check_issue(issue) }
     end
     # :nocov:
+  end
+end
+
+
+# kill Timeout module for debug bug in Rubymine
+if $LOADED_FEATURES.any? { |f| f.include? 'debase' }
+  module Timeout
+    def timeout(sec, klass=nil)
+      yield(sec)
+    end
+
+    module_function :timeout
   end
 end
