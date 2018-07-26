b41d0536cd0de1db5d93465480b6406d3f47e074
sergey <sergey.matveev@onetwotrip.com>
2018-07-26 16:17:03 +0300

    * New logs format * After detected issue problems tasks will reopen it

--- lib/scenarios/checkconflicts.rb
+++ lib/scenarios/checkconflicts.rb
@@ -19,23 +19,24 @@ module Scenarios
           commit          = BITBUCKET.repo(pr.repo_owner, pr.repo_slug).commit(commit_id)
           status_of_build = commit.build_statuses.collect.last
           if status_of_build.state.upcase.include? 'FAILED'
-            LOGGER.info "Detected build status error in #{issue_task.key}. Writing comment in ticket...".red
+            puts "Detected build status error in #{issue_task.key}. Writing comment in ticket...".red
+            puts "#{issue_task.key}: https://bitbucket.org/OneTwoTrip/#{pr.repo_slug}/pull-requests/#{pr.id}".red
+            puts "Writing message in #{issue_task.key}".red
             issue_task.post_comment 'Ticket has build error status, pls check it'
+            puts "Reopen ticket: #{issue_task.key}".red
+            issue_task.transition 'Reopen' if issue_task.status.name != 'Reopen'
           end
           conflict_flag     = diff_in_pr.include? '<<<<<<<'
-          log_string        = "Status of pullrequest #{pr.title} is #{status_of_build.name}:#{status_of_build.state} and ".green
-          conflict_flag_log = "conflict_flag is #{conflict_flag}".green
           if conflict_flag
-            LOGGER.info "Find conflicts in #{issue_task.key}. Writing comment in ticket..."
-            conflict_flag_log = "conflict_flag is #{conflict_flag}".red
+            puts "Find conflicts in #{issue_task.key}. Writing comment in ticket...".red
+            puts "#{issue_task.key}: https://bitbucket.org/OneTwoTrip/#{pr.repo_slug}/pull-requests/#{pr.id}".red
+            puts "Writing message in #{issue_task.key}".red
             issue_task.post_comment 'After last release this issue started to have merge conflicts. Please fix it'
-            LOGGER.info "Finished writing merge conflict message in #{issue_task.key}"
+            puts "Reopen ticket: #{issue_task.key}".red
+            issue_task.transition 'Reopen' if issue_task.status.name != 'Reopen'
           end
-
-          log_string += conflict_flag_log + " with link https://bitbucket.org/OneTwoTrip/#{pr.repo_slug}/pull-requests/#{pr.id}".green
-          LOGGER.info log_string
         rescue StandardError => error
-          LOGGER.info "There is error occured with ticket #{issue_task.key}: #{error.message}".red
+          puts "There is error occurred with ticket #{issue_task.key}: #{error.message}".red
         end
       end
     end
@@ -84,7 +85,7 @@ module Scenarios
 
       LOGGER.info "Check conflicts in tasks from filter #{release_filter}".green
       issues = release_filter && find_by_filter(client.Issue, release_filter)
-      LOGGER.info 'Start check issues'.green
+      LOGGER.info "Start check #{issues.count} issues".green
       issues.each { |issue| check_issue(issue) }
     end
     # :nocov:
