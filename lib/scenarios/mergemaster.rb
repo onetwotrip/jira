d71bd6cad5f3d832d5b8836b092c173236a5f018
Sergey <hroft356@mail.ru>
2019-09-04 09:59:10 +0300

    add message about conflict (#217)

--- lib/scenarios/mergemaster.rb
+++ lib/scenarios/mergemaster.rb
@@ -9,52 +9,57 @@ module Scenarios
 
     def run
       LOGGER.info "Starting merge_master for #{SimpleConfig.jira.issue}"
-      jira  = JIRA::Client.new SimpleConfig.jira.to_h
-      issue = jira.Issue.find(SimpleConfig.jira.issue)
+      jira       = JIRA::Client.new SimpleConfig.jira.to_h
+      issue      = jira.Issue.find(SimpleConfig.jira.issue)
+      fail_merge = {}
       issue.post_comment <<-BODY
       {panel:title=Release notify!|borderStyle=dashed|borderColor=#ccc|titleBGColor=#E5A443|bgColor=#F1F3F1}
         Запущено подтягивание мастеров в ветки задачи(!)
         Ожидайте сообщение о завершении
       {panel}
       BODY
-      begin
-        issue.related['branches'].each do |branch|
-          LOGGER.info "Start work with branch: #{branch['name']}"
-          repo_path = git_repo(branch['repository']['url'])
-          # Prepare repo
-          repo_path.checkout('master')
-          repo_path.pull
-          repo_path.chdir do
-            `git fetch --prune`
-          end
-          # Check for case when branch has correct name, but was deleted from issue
-          unless repo_path.is_branch? branch['name']
-            LOGGER.error "[SKIP] #{branch['repository']['name']}/#{branch['name']} - branch doesn't exist"
-            next
-          end
+
+      issue.related['branches'].each do |branch|
+        LOGGER.info "Start work with branch: #{branch['name']}"
+        repo_path = git_repo(branch['repository']['url'])
+        # Prepare repo
+        repo_path.checkout('master')
+        repo_path.pull
+        repo_path.chdir do
+          `git fetch --prune`
+        end
+        # Check for case when branch has correct name, but was deleted from issue
+        unless repo_path.is_branch? branch['name']
+          LOGGER.error "[SKIP] #{branch['repository']['name']}/#{branch['name']} - branch doesn't exist"
+          next
+        end
+        begin
           with repo_path do
             checkout branch['name']
             pull('origin', branch['name'])
             pull('origin', 'master')
             push(repo_path.remote('origin'), branch['name'])
           end
+        rescue StandardError => e
+          LOGGER.error "Не подтянуть мастера, ошибка: #{e.message}, трейс:\n\t#{e.backtrace.join("\n\t")}"
+          fail_merge[branch['repository']['name'].to_sym] = branch['name']
         end
-      rescue StandardError => e
+      end
+      if fail_merge.empty?
         issue.post_comment <<-BODY
         {panel:title=Release notify!|borderStyle=dashed|borderColor=#ccc|titleBGColor=#E5A443|bgColor=#F1F3F1}
-         Не удалось подтянуть мастера (x)
-         Подробности в логе таски https://jenkins.twiket.com/view/RELEASE/job/merge_master/
+          Ветки обновлены! (/)
         {panel}
         BODY
-        LOGGER.error "Не подтянуть мастера, ошибка: #{e.message}, трейс:\n\t#{e.backtrace.join("\n\t")}"
-        issue.transition 'Merge Fail'
-        exit(1)
+      else
+        issue.post_comment <<-BODY
+            {panel:title=Build status error|borderStyle=dashed|borderColor=#ccc|titleBGColor=#F7D6C1|bgColor=#FFFFCE}
+               Не удалось подтянуть мастера (x)
+                *Причина:* Merge conflict
+                *Ветки:* #{fail_merge}
+            {panel}
+        BODY
       end
-      issue.post_comment <<-BODY
-      {panel:title=Release notify!|borderStyle=dashed|borderColor=#ccc|titleBGColor=#E5A443|bgColor=#F1F3F1}
-        Ветки обновлены! (/)
-      {panel}
-      BODY
     end
   end
 end
