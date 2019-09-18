5023f0b9e4d7227ddc1e9d0e896ef8a486b451d8
Sergey <hroft356@mail.ru>
2019-09-18 16:42:50 +0300

    Qa 1643 android label release (#220)
    * add checks for label in release ticket
    
    * add new logic with labels for mobile release + add new basic auth method for create pullrequests

--- lib/git/bitbucket.rb
+++ lib/git/bitbucket.rb
@@ -24,6 +24,30 @@ module Git
       end
     end
 
+    # :nocov:
+    def new_create_pullrequest(src = current_branch, destination = 'master')
+      request = { title: "#{src} #{remote.url.repo}",
+                  source: { branch: { name: src },
+                            repository: { full_name: remote.url.repo } },
+                  destination: { branch: { name: destination } } }
+      begin
+        url = "https://bitbucket.org/!api/2.0/repositories/#{remote.url.repo}/pullrequests"
+        LOGGER.info "POST #{url}"
+        RestClient::Request.execute(
+          method:   :post,
+          url:      url,
+          user: SimpleConfig.bitbucket[:username],
+          password: SimpleConfig.bitbucket[:password],
+          payload: request.to_json,
+          headers: { content_type: :json }
+        )
+      rescue StandardError => e
+        LOGGER.fatal "Pullrequest didn't create"
+        LOGGER.fatal "Error: #{e}; URL: #{url}; PARAMS: #{request}"
+        exit(1)
+      end
+    end
+
     def decline_pullrequest(username = nil, password = nil, pull_request_id = '')
       url = "https://#{username}:#{password}@api.bitbucket.org/2.0/repositories/#{remote.url.repo}/pullrequests/#{pull_request_id}/decline" # rubocop:disable Metrics/LineLength
       RestClient.post url, content_type: :json
