d4b4ae19c0f10489a336d1080846ca7fc8514c8a
Sergey Matveev <sergey.matveev@onetwotrip.com>
2019-10-25 10:36:56 +0300

    add logs

--- lib/scenarios/checkcodeowners.rb
+++ lib/scenarios/checkcodeowners.rb
@@ -37,9 +37,13 @@ module Scenarios
         owners_config = {}
         # Get PR diff and owners_config
         with pr_repo do
-          diff_stats         = get_pullrequests_diffstats(pr_id)
-          owners_config_path = File.expand_path('../../../', __FILE__)
-          owners_config      = YAML.load_file "#{owners_config_path}/bin/#{remote.url.repo}/owners.yml"
+          LOGGER.info 'Try to diff stats from BB'
+          diff_stats = get_pullrequests_diffstats(pr_id)
+          LOGGER.info 'Success!'
+          LOGGER.info "Try to get owners.yml file for project #{remote.url.repo}"
+          owners_config_path = "#{File.expand_path('../../../', __FILE__)}/bin/#{remote.url.repo}/owners.yml"
+          owners_config      = YAML.load_file owners_config_path
+          LOGGER.info "Success!Got file from #{owners_config_path}"
         end
 
         modified_files = get_modified_links(diff_stats)
@@ -50,7 +54,9 @@ module Scenarios
 
         # Add info and new reviewers in PR
         with pr_repo do
+          LOGGER.info 'Try to add reviewers to PR'
           add_info_in_pullrequest(pr_id, 'Description without reviewers ok', new_reviewers_list, pr_name)
+          LOGGER.info 'Success! Everything fine!'
         end
       end
     end
@@ -61,15 +67,18 @@ module Scenarios
     end
 
     def get_modified_links(diff_stats)
+      LOGGER.info 'Try to get modified files'
       result   = []
       statuses = %w[modified removed]
       diff_stats[:values].each do |diff|
         result << diff[:old][:path] if statuses.include? diff[:status]
       end
+      LOGGER.info 'Success!'
       result
     end
 
     def get_reviewers_id(reviewers, pr_repo)
+      LOGGER.info 'Try to get current reviewers id'
       result    = []
       reviewers = [reviewers] unless reviewers.is_a? Array
       with pr_repo do
@@ -78,20 +87,24 @@ module Scenarios
           result << get_reviewer_info(user['name'].sub(' ', '+')).first[:mention_id]
         end
       end
+      LOGGER.info "Success! Result: #{result}"
       result
     end
 
     def get_owners(owners_config, diff)
+      LOGGER.info 'Try to get owners ids'
       result = {}
       diff.each do |item|
         owners_config.each do |product|
           result[product[0]] = product[1]['owners'] if product[1]['files'].include? item
         end
       end
+      LOGGER.info "Success! Result: #{result}"
       result
     end
 
     def prepare_new_reviewers_list(old_reviewers, owners, author_id)
+      LOGGER.info 'Try to prepare reviewers list for add to PR'
       result = []
       old_reviewers.each { |reviewer| result << { account_id: reviewer } }
       owners.each do |owner|
@@ -99,6 +112,7 @@ module Scenarios
           result << { account_id: id } unless id == author_id
         end
       end
+      LOGGER.info "Success! Result: #{result}"
       result
     end
   end
