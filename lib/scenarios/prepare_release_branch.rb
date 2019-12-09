7851f2c79f3e313dff3e978371fca06972f6f333
Sergey <hroft356@mail.ru>
2019-12-09 11:19:32 +0300

    Qa 1789 prepare release branch (#236)
    * add IOS for update branch after develop changed
    
    * add 2 task for feature CD

---
+++ lib/scenarios/prepare_release_branch.rb
@@ -0,0 +1,28 @@
+require 'scenarios'
+module Scenarios
+  ##
+  # Check is auto test release branch exist scenario
+  class PrepareReleaseBranch
+    def run
+      jira  = JIRA::Client.new SimpleConfig.jira.to_h
+      issue = jira.Issue.find(SimpleConfig.jira.issue)
+      LOGGER.info "Try to get all release branch from ticket #{issue.key}"
+      release_branch = ''
+      issue.related['branches'].each do |branch|
+        if branch['repository']['name'].include? 'avia_api_rspec' # rubocop:disable Style/Next
+          LOGGER.info "Found auto test branch: #{branch['name']}"
+          release_branch = branch['name']
+          break
+        end
+      end
+      if release_branch.empty?
+        default_branch = 'master'
+        LOGGER.warn "Auto test branch din't find, so i will take '#{default_branch}' branch by default"
+        release_branch = default_branch
+      end
+      LOGGER.info 'Prepare file with info'
+      Ott::Helpers.export_to_file("TEST_BRANCH=#{release_branch}", 'test_branch')
+      LOGGER.info 'Success!'
+    end
+  end
+end
