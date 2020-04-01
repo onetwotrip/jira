57be08fdfa2b3d0cb211e02c68be33631be6ce8b
Sergey <hroft356@mail.ru>
2020-04-01 11:48:03 +0300

    QA-1965 add logic for prepare android release branch (#260)
    Co-authored-by: s.matveev <s.matveev@qiwi.ru>

--- lib/scenarios/prepare_release_branch.rb
+++ lib/scenarios/prepare_release_branch.rb
@@ -6,6 +6,19 @@ module Scenarios
     def run
       jira  = JIRA::Client.new SimpleConfig.jira.to_h
       issue = jira.Issue.find(SimpleConfig.jira.issue)
+
+      release_branch = if ENV['ADR']
+        prepare_android_branch(issue)
+      else
+        prepare_tests_branch(issue)
+      end
+
+      LOGGER.info 'Prepare file with info'
+      Ott::Helpers.export_to_file("BRANCH=#{release_branch}", 'test_branch')
+      LOGGER.info 'Success!'
+    end
+
+    def prepare_tests_branch(issue)
       LOGGER.info "Try to get all release branch from ticket #{issue.key}"
       release_branch = ''
       issue.related['branches'].each do |branch|
@@ -20,9 +33,26 @@ module Scenarios
         LOGGER.warn "Auto test branch din't find, so i will take '#{default_branch}' branch by default"
         release_branch = default_branch
       end
-      LOGGER.info 'Prepare file with info'
-      Ott::Helpers.export_to_file("BRANCH=#{release_branch}", 'test_branch')
-      LOGGER.info 'Success!'
+      release_branch
+    end
+
+    def prepare_android_branch(issue)
+      LOGGER.info "Try to get all branch from ticket #{issue.key}"
+      release_branch = ''
+      issue.related['branches'].each do |branch|
+        if branch['repository']['name'].include? 'android_ott' # rubocop:disable Style/Next
+          LOGGER.info "Found android branches. Try to find release branch"
+          next unless branch['name'].include? 'release/'
+          LOGGER.info "Found release branches: #{branch['name']}"
+          release_branch = branch['name']
+          break
+        end
+      end
+      if release_branch.empty?
+        LOGGER.error "Can't find any release branch in #{issue.key}"
+        exit(1)
+      end
+      release_branch
     end
   end
 end
