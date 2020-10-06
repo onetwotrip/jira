de7f7625948a4326316b1e576376df5ebc15aa4c
Sergey Matveev <sergey.matveev@onetwotrip.com>
2020-10-06 15:37:40 +0300

    update_mobile_branches refactor + add logs

--- lib/scenarios/update_mobile_branches.rb
+++ lib/scenarios/update_mobile_branches.rb
@@ -15,11 +15,56 @@ module Scenarios
       instance
     end
 
+    def run
+      adr_filter = 30_361
+      ios_filter = 30_421
+      # Get all tickets
+      jira = JIRA::Client.new SimpleConfig.jira.to_h
+      # noinspection RubyArgCount
+      issue = jira.Issue.find(SimpleConfig.jira.issue)
+      update_issue(issue)
+      LOGGER.info("Start work after #{issue.key} was merged")
+      project_name = issue.fields['project']['key']
+
+      # abort('IOS ticket was merged, so i will skip this task. Only ADR project supports this feature') if project_name.include?('IOS')
+
+      filter = case project_name
+                 when 'ADR'
+                   LOGGER.info "Took ADR filter: #{adr_filter}"
+                   adr_filter
+                 when 'IOS'
+                   LOGGER.info "Took IOS filter: #{ios_filter}"
+                   ios_filter
+                 else
+                   abort('Only IOS or ADR projects support this cool feature, so i will skip this task')
+               end
+
+      LOGGER.info "Try to find all tasks from filter #{filter}".green
+      issues = find_by_filter(jira.Issue, filter)
+      LOGGER.info "Found #{issues.count} issues".green
+      count_max = issues.count
+      counter = 1
+
+      issues.each do |i|
+        LOGGER.info "Work with #{i.key} (#{counter}/#{count_max})"
+        unless i.fields['fixVersions'].empty?
+          LOGGER.warn "Issue #{i.key} contains fixVersions, so this is release ticket and i will skip update branch"
+          next
+        end
+        update_issue(i)
+        counter += 1
+      end
+    end
+
     def update_issue(issue)
       pullrequests = issue.pullrequests(SimpleConfig.git.to_h).filter_by_status('OPEN')
       LOGGER.info "Found #{pullrequests.prs.count} pullrequests".green
       pullrequests.each do |pr| # rubocop:disable Metrics/BlockLength
-        next unless pr.pr['destination']['branch'].include? 'develop'
+        unless pr.pr['destination']['branch'].include? 'develop'
+          LOGGER.warn "Found PR from #{pr.pr['source']['branch']} to #{pr.pr['destination']['branch']}. SKIP!"
+          next
+        end
+        LOGGER.info "Found PR from #{pr.pr['source']['branch']} to #{pr.pr['destination']['branch']}. ok!".green
         begin
           pr_repo = git_repo(pr.pr['destination']['url'])
           # Prepare repo
@@ -55,46 +100,6 @@ module Scenarios
           issue.transition 'Conflicts'
           next
         end
-      end
-    end
-
-    def run
-      adr_filter = 30_361
-      ios_filter = 30_421
-      # Get all tickets
-      jira = JIRA::Client.new SimpleConfig.jira.to_h
-      # noinspection RubyArgCount
-      issue = jira.Issue.find(SimpleConfig.jira.issue)
-      LOGGER.info("Start work after #{issue.key} was merged")
-      project_name = issue.fields['project']['key']
-
-      # abort('IOS ticket was merged, so i will skip this task. Only ADR project supports this feature') if project_name.include?('IOS')
-
-      filter = case project_name
-               when 'ADR'
-                 LOGGER.info "Took ADR filter: #{adr_filter}"
-                 adr_filter
-               when 'IOS'
-                 LOGGER.info "Took IOS filter: #{ios_filter}"
-                 ios_filter
-               else
-                 abort('Only IOS or ADR projects support this cool feature, so i will skip this task')
-               end
-
-      LOGGER.info "Try to find all tasks from filter #{filter}".green
-      issues = find_by_filter(jira.Issue, filter)
-      LOGGER.info "Found #{issues.count} issues".green
-      count_max = issues.count
-      counter   = 1
-
-      issues.each do |i|
-        LOGGER.info "Work with #{i.key} (#{counter}/#{count_max})"
-        unless i.fields['fixVersions'].empty?
-          LOGGER.warn "Issue #{i.key} contains fixVersions, so this is release ticket and i will skip update branch"
-          next
-        end
-        update_issue(i)
-        counter += 1
       end
     end
   end
