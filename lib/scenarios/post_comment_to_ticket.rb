6b783c1745f7431e8e4d9ccb3ccc40cb212fe6f1
s.matveev <s.matveev@qiwi.ru>
2020-11-18 15:50:49 +0300

    QA-2398

--- lib/scenarios/post_comment_to_ticket.rb
+++ lib/scenarios/post_comment_to_ticket.rb
@@ -2,5 +2,11 @@ module Scenarios
   ##
   # Post comment to ticket
   class PostCommentToTicket
+    def run
+      jira = JIRA::Client.new SimpleConfig.jira.to_h
+      issue = jira.Issue.find(SimpleConfig.jira.issue)
+      LOGGER.info "Try to post message: '#{ENV['MESSAGE']}' into ticket #{issue.key}"
+      issue.post_comment ENV['MESSAGE']
+    end
   end
 end
