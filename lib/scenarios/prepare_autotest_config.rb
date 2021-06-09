fd7e263459f192b6965b6464c68936bff2e47e1f
Sergey <hroft356@mail.ru>
2021-06-09 10:33:03 +0300

    Qa 2613 autotest prepare task (#308)
    * QA-2613 add prepare_autotest_config
    
    * Update permissions
    
    * QA-2613
    
    * QA-2613 add prepare_autotest_config.rb
    
    Co-authored-by: s.matveev <s.matveev@qiwi.ru>
    Co-authored-by: Alexey Sutiagin <alexey.sutiagin@onetwotrip.com>
    Co-authored-by: Sergey Matveev <sergey.matveev@onetwotrip.com>

---
+++ lib/scenarios/prepare_autotest_config.rb
@@ -0,0 +1,28 @@
+module Scenarios
+
+  # Prepare config for autotest branch + tags for execute
+  class PrepareAutotestConfig
+
+    def run
+      jira = JIRA::Client.new SimpleConfig.jira.to_h
+      issue = jira.Issue.find(SimpleConfig.jira.issue)
+      LOGGER.info("Start work with #{Ott::Helpers.jira_link(issue.key)}")
+      LOGGER.info('Start prepare release branch')
+      branch = Scenarios::PrepareReleaseBranch.new.run
+      LOGGER.info("Success! #{branch}")
+      LOGGER.info('Try to get repo list')
+      repos = Scenarios::PrepareAllRepoList.new.run(release_skip: true)
+      LOGGER.info("Success! repo: #{repos}")
+      config = "#{branch};ARGS="
+      if repos.empty?
+        LOGGER.error("Repo is empty, but it shouldn't")
+        exit(1)
+      else
+        repos.split(',').each { |repo| config += " --tag #{repo}," }
+      end
+      LOGGER.info("Final config is: #{config}")
+      Ott::Helpers.export_to_file(config, 'autotest_config')
+    end
+
+  end
+end
