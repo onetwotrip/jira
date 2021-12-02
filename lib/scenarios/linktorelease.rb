c16be2d02630bea2c1949d046a5d2b7c9cc96de9
Sergey Matveev <sergey.matveev@onetwotrip.com>
2021-12-02 11:14:38 +0300

    QA-2962 update linktorelease.rb

--- lib/scenarios/linktorelease.rb
+++ lib/scenarios/linktorelease.rb
@@ -20,29 +20,18 @@ module Scenarios
 
       filter_config = JSON.parse(ENV['RELEASE_FILTER'])
       client = JIRA::Client.new SimpleConfig.jira.to_h
-      release_issue = client.Issue.find(SimpleConfig.jira.issue)
-      LOGGER.info Ott::Helpers.jira_link(release_issue.key).to_s
+      issue = client.Issue.find(SimpleConfig.jira.issue)
+      LOGGER.info Ott::Helpers.jira_link(issue.key).to_s
 
-      project_name = release_issue.fields['project']['key']
-      release_name = release_issue.fields['summary'].upcase
-      release_issue_number = release_issue.key
-      components = release_issue.fields['components']
-      component = if components.count > 1
-                    LOGGER.error "Found more than 1 components: #{components.count} Should be only 1"
-                    release_issue.post_comment <<-BODY
-                      {panel:title=Release notify!|borderStyle=dashed|borderColor=#ccc|titleBGColor=#E5A443|bgColor=#F1F3F1}
-                        Релиз не должен содержать больше чем 1 компонент (x)
-                      {panel}
-                    BODY
-                    exit
-                  else
-                    components
-                  end
+      project_name = issue.fields['project']['key']
+      release_name = issue.fields['summary'].upcase
+      release_issue_number = issue.key
+      assemble = issue.fields['assemble'] || ''
 
       # Check project exist in filter_config
       if filter_config[project_name].nil?
         message = "I can't work with project '#{project_name.upcase}'. Pls, contact administrator to feedback"
-        release_issue.post_comment <<-BODY
+        issue.post_comment <<-BODY
           {panel:title=Release notify!|borderStyle=dashed|borderColor=#ccc|titleBGColor=#E5A443|bgColor=#F1F3F1}
             #{message} (x)
           {panel}
@@ -65,16 +54,20 @@ module Scenarios
       LOGGER.info "Release type: #{release_type}"
       release_filter = filter_config[project_name][release_type]
 
-      release_filter = if component.empty?
-                         "#{release_filter} AND (component not in('B2B_Android','B2B_IOS') or component is EMPTY)"
-                       else
-                         "#{release_filter} AND (component = #{component.first['name']}"
-                       end
+      if mobile_project?(issue.key)
+        release_filter = if assemble.empty?
+                           "#{release_filter} AND (assemble = b2c_ott OR assemble is EMPTY)"
+                         else
+                           "#{release_filter} AND assemble = #{assemble}"
+                         end
+      else
+        release_filter
+      end
 
       # Check release filter
       if release_filter.nil? || release_filter.empty?
         message = "I don't find release filter for jira project: '#{project_name.upcase}' and release_type: #{release_type}"
-        release_issue.post_comment <<-BODY
+        issue.post_comment <<-BODY
           {panel:title=Release notify!|borderStyle=dashed|borderColor=#ccc|titleBGColor=#E5A443|bgColor=#F1F3F1}
             #{message} (x)
           {panel}
@@ -90,7 +83,7 @@ module Scenarios
       # Check issues not empty
       if issues.empty?
         LOGGER.warn "Release filter: #{release_filter} doesn't contain any issues"
-        release_issue.post_comment <<-BODY
+        issue.post_comment <<-BODY
           {panel:title=Release notify!|borderStyle=dashed|borderColor=#ccc|titleBGColor=#E5A443|bgColor=#F1F3F1}
             Фильтр #{release_filter} не содержит задач (x)
           {panel}
@@ -101,20 +94,20 @@ module Scenarios
       end
 
       # Message about count of release candidate issues
-      release_issue.post_comment <<-BODY
+      issue.post_comment <<-BODY
           {panel:title=Release notify!|borderStyle=dashed|borderColor=#ccc|titleBGColor=#E5A443|bgColor=#F1F3F1}
             Тикетов будет прилинковано: #{issues.count} (!)
           {panel}
       BODY
 
-      issues.each do |issue|
-        issue.link(release_issue_number)
+      issues.each do |i|
+        i.link(release_issue_number)
       end
 
       unless %w[ADR IOS].any? { |p| release_issue_number.include? p }
         release_labels = []
-        issues.each do |issue|
-          issue.related['branches'].each do |branch|
+        issues.each do |i|
+          i.related['branches'].each do |branch|
             release_labels << branch['repository']['name'].to_s
           end
         end
@@ -122,16 +115,20 @@ module Scenarios
         release_labels.uniq!
 
         LOGGER.info "Add labels: #{release_labels} to release #{release_name}"
-        release_issue.save(fields: { labels: release_labels })
-        release_issue.fetch
+        issue.save(fields: { labels: release_labels })
+        issue.fetch
       end
 
       # Message about done
-      release_issue.post_comment <<-BODY
+      issue.post_comment <<-BODY
           {panel:title=Release notify!|borderStyle=dashed|borderColor=#ccc|titleBGColor=#E5A443|bgColor=#F1F3F1}
             Формирование релиза закончено (/)
           {panel}
       BODY
+    end
+
+    def mobile_project?(key)
+      %w[ADR IOS].include? key.split('-').first
     end
   end
 end
