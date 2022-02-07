73525012319bc9f640f3075353355ee045ff2004
Sergey Matveev <sergey.matveev@onetwotrip.com>
2022-02-07 16:32:16 +0300

    hide slack webhook url to ENV

--- lib/scenarios/postaction_mobile_release.rb
+++ lib/scenarios/postaction_mobile_release.rb
@@ -124,7 +124,7 @@ module Scenarios
 
       # Sync Android b2c/b2b repos
       if Ott::Helpers.mobile_project?(issue.key) && is_master_updated && !issue.key.include?('IOS')
-        webhook_url = 'https://hooks.slack.com/services/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'
+        webhook_url = ENV['SLACK_WEB_HOOK']
         if is_b2c_project
           LOGGER.info 'B2C project was updated'
           LOGGER.info 'Make PR из B2c master -> B2B develop (report slack channel)'
