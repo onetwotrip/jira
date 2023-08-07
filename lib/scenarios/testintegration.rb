c3fe7d3f79ddb4ca7f8ae3fc8c6c9390e3e366e8
Ivan Abramov <ivan.abramov@onetwotrip.com>
2023-08-07 14:28:53 +0300

    QA-3878: rollback

--- lib/scenarios/testintegration.rb
+++ lib/scenarios/testintegration.rb
@@ -3,29 +3,20 @@ module Scenarios
   # TestIntegration scenario
   class TestIntegration
     def run
-      LOGGER.info "Starting #{self.class.name} for #{SimpleConfig.jira.issue}"
-      jira  = JIRA::Client.new SimpleConfig.jira.to_h
+      LOGGER.info "Starting run tests for #{SimpleConfig.jira.issue}"
+
+      jira = JIRA::Client.new SimpleConfig.jira.to_h
       issue = jira.Issue.find(SimpleConfig.jira.issue)
+
       LOGGER.info Ott::Helpers.jira_link(issue.key).to_s
-      LOGGER.info 'Get all labels'
-      labels = issue.labels
 
-      LOGGER.info "Issue already has labels: #{labels} - I will add all new one to current" unless labels.empty?
-
-      issue.api_pullrequests.each do |br|
-        LOGGER.info("Repo: #{br.repo_slug}")
-        labels << br.repo_slug
-      end
-
-      unless ENV['LABELS'].nil?
-        LOGGER.info "Found some additional labels: #{ENV['LABELS']} - I will add them to issue #{issue.key}"
-        ENV['LABELS'].sub(' ', '').split(',').each do |it|
-          labels << it
-        end
-      end
-
-      LOGGER.info "Add labels: #{labels.uniq} to issue #{issue.key}"
-      issue.save(fields: { labels: labels.uniq })
+      issue.post_comment <<-BODY
+      {panel:title=Release notify!|borderStyle=dashed|borderColor=#ccc|titleBGColor=#E5A443|bgColor=#F1F3F1}
+        Тестовое сообщение(!)
+        #{ENV['BUILD_URL']} 
+        Ожидайте сообщение о завершении
+      {panel}
+      BODY
     end
   end
 end
