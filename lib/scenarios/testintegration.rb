38934238a17753dcd2d1079baf6d4ef610634170
ivan.abramov <ivan.abramov@onetwotrip.com>
2023-08-08 17:24:43 +0300

    QA-3878: research

--- lib/scenarios/testintegration.rb
+++ lib/scenarios/testintegration.rb
@@ -16,27 +16,26 @@ module Scenarios
       issue_links.each do |link|
         inward_issue = link['inwardIssue']
         outward_issue = link['outwardIssue']
-        key = inward_issue ? inward_issue['key'] : outward_issue['key']
+        issue_name = inward_issue ? inward_issue['key'] : outward_issue['key']
         id = inward_issue ? inward_issue['id'] : outward_issue['id']
 
         nestedIssue = jira.Issue.find(id)
         nested_issue_links = nestedIssue.fields['issuelinks']
 
-        nested_name = 0
+        type = 0
         nested_key = 0
 
         nested_issue_links.each do |nested_link|
           nested_inward_issue = nested_link['inwardIssue']
           nested_outward_issue = nested_link['outwardIssue']
-          nested_name = nested_link['type']['name']
+          type = nested_link['type']['name']
 
-          # nested_id = nested_inward_issue ? nested_inward_issue['id'] : nested_outward_issue['id']
           nested_key = nested_inward_issue ? nested_inward_issue['key'] : nested_outward_issue['key']
         end
 
-        object = { key: key, nested_issues: {
-          nested_name: nested_name,
-          nested_key: nested_key,
+        object = { issue: "https://onetwotripdev.atlassian.net/browse/#{issue_name}", nested_issues: {
+          type: type,
+          nested_key: "https://onetwotripdev.atlassian.net/browse/#{nested_key}",
         } }
 
         issues << object
@@ -44,13 +43,12 @@ module Scenarios
 
       puts issues.to_json
 
-      # issue.post_comment <<-BODY
-      # {panel:title=Release notify!|borderStyle=dashed|borderColor=#ccc|titleBGColor=#E5A443|bgColor=#F1F3F1}
-      #   Тестовое сообщение(!)
-      #   #{ENV['BUILD_URL']}
-      #   Ожидайте сообщение о завершении
-      # {panel}
-      # BODY
+      issue.post_comment <<-BODY
+      {panel:title=Release notify!|borderStyle=dashed|borderColor=#ccc|titleBGColor=#E5A443|bgColor=#F1F3F1}
+        Внимание, зависимость задач(!)
+        #{issues.to_json}
+      {panel}
+      BODY
     end
   end
 end
