eea0b9c3ca00ac350b2a0516d8e604edf7c42007
ivan.abramov <ivan.abramov@onetwotrip.com>
2023-08-08 10:34:01 +0300

    QA-3878: research

--- lib/scenarios/testintegration.rb
+++ lib/scenarios/testintegration.rb
@@ -5,13 +5,13 @@ module Scenarios
     def run
       LOGGER.info "Starting for #{SimpleConfig.jira.issue}"
 
-      jira = JIRA::Client.new SimpleConfig.jira.to_h
-      issue = jira.Issue.find(SimpleConfig.jira.issue)
+      # jira = JIRA::Client.new SimpleConfig.jira.to_h
+      # issue = jira.Issue.find(SimpleConfig.jira.issue)
 
-      inward_issues = issue.inward_issues
-      LOGGER.info "Found #{inward_issues} inward issues"
+      link = client.IssueLink.build
 
-      LOGGER.info Ott::Helpers.jira_link(issue.key).to_s
+      puts link
+
 
       # issue.post_comment <<-BODY
       # {panel:title=Release notify!|borderStyle=dashed|borderColor=#ccc|titleBGColor=#E5A443|bgColor=#F1F3F1}
