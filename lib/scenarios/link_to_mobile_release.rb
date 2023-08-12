ebea97f5ca3b631a44eed01685bec432edf3be63
Viktor Usachev <viktor.usachev@onetwotrip.com>
2023-08-12 14:59:52 +0300

    QA-4267 first commit

--- lib/scenarios/link_to_mobile_release.rb
+++ lib/scenarios/link_to_mobile_release.rb
@@ -41,7 +41,7 @@ module Scenarios
 
       created_releases = find_by_filter(isss, 31159)
       # created_releases = issue.jql("issuetype = Release and status != Done and \"App[Dropdown]\" = b2c_ott", max_results: 100)
-      puts created_releases
+      puts created_releases.to_json
 
       # # собираем список
       # issue_links.each do |item|
