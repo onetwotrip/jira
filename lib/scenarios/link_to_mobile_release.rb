5575812dfc896fd44949c60b9fc2c52422782528
Viktor Usachev <viktor.usachev@onetwotrip.com>
2023-08-12 15:12:57 +0300

    QA-4267 first commit

--- lib/scenarios/link_to_mobile_release.rb
+++ lib/scenarios/link_to_mobile_release.rb
@@ -46,7 +46,7 @@ module Scenarios
       # }
 
 
-      created_releases = isss.jql(%(issuetype = Release and status != Done and "App[Dropdown]" = b2c_ott"))
+      created_releases = client.Issue.jql(%("App[Dropdown]" = b2c_ott"))
       # created_releases = issue.jql("issuetype = Release and status != Done and \"App[Dropdown]\" = b2c_ott", max_results: 100)
       puts created_releases.to_json
 
