22b72ad54fd6090be5644e9397d28c81828b95ad
Viktor Usachev <viktor.usachev@onetwotrip.com>
2023-08-12 15:54:00 +0300

    QA-4267 first commit

--- lib/scenarios/link_to_mobile_release.rb
+++ lib/scenarios/link_to_mobile_release.rb
@@ -35,7 +35,8 @@ module Scenarios
       puts apps
 
       # created_releases = client.Issue.jql(%("App[Dropdown]" = b2c_ott"))
-      created_releases = client.Issue.jql(%(project = ios and issuetype = Release and status != Done and "App[Dropdown]" = #{apps}), max_results: 100)
+      # created_releases = client.Issue.jql(%(project = ios and issuetype = Release and status != Done and "App[Dropdown]" = #{apps}), max_results: 100)
+      created_releases = client.Issue.jql(%(project = android and issuetype = Release and status = Rejected and "App[Dropdown]" = #{apps}), max_results: 100)
       # created_releases = issue.jql("issuetype = Release and status != Done and \"App[Dropdown]\" = b2c_ott", max_results: 100)
       puts created_releases.to_json
 
