a8b3561f0be76ea27176246f1c5fcda523b36f62
Viktor Usachev <viktor.usachev@onetwotrip.com>
2023-09-06 14:43:48 +0300

    added message in script

--- lib/scenarios/link_to_mobile_release.rb
+++ lib/scenarios/link_to_mobile_release.rb
@@ -34,18 +34,19 @@ module Scenarios
       # Получаем данные тикета
       client = JIRA::Client.new SimpleConfig.jira.to_h
       issue  = client.Issue.find(SimpleConfig.jira.issue)
-      LOGGER.info Ott::Helpers.jira_link(issue.key).to_s
+      issue_number = issue.key
+      LOGGER.info Ott::Helpers.jira_link(issue_number).to_s
 
       # Уточняем какой тип проекта будет проверятся Андроид или IOS
-      if issue.key[0..2].eql?('IOS') || issue.key[0..2].eql?('ADR')
-        project = issue.key.include?('IOS') ? 'ios' : 'android'
+      if issue_number[0..2].eql?('IOS') || issue_number[0..2].eql?('ADR')
+        project = issue_number.include?('IOS') ? 'ios' : 'android'
         # Получаем значения поля apps из релиза
         # apps = issue.fields['customfield_12166']['value']
         apps = issue.fields['customfield_12208']['value']
 
         # Проверяем есть ли релизы AND/IOS с такими значениями и не закрытые
         # Производим поиск открытых релизов
-        created_releases = client.Issue.jql(%(project = #{project} and issuetype = Release and status not in (Rejected, Done) and "App[Dropdown]" = #{apps} and issue != #{issue.key}), max_results: 100)
+        created_releases = client.Issue.jql(%(project = #{project} and issuetype = Release and status not in (Rejected, Done) and "App[Dropdown]" = #{apps} and issue != #{issue_number}), max_results: 100)
 
         # if created_releases == []
         # TODO: для проверки создания тикета
@@ -110,7 +111,7 @@ module Scenarios
           issues_apps_type_uniq.each do |app_uniq|
             puts "Проверяем #{app_uniq}"
             created_releases = client.Issue.jql(
-              %(project = #{project} and issuetype = Release and status not in (Rejected, Done) and "App[Dropdown]" = #{app_uniq} and issue != #{issue.key}), max_results: 100)
+              %(project = #{project} and issuetype = Release and status not in (Rejected, Done) and "App[Dropdown]" = #{app_uniq} and issue != #{issue_number}), max_results: 100)
             puts "created_releases, #{created_releases}"
             puts created_releases.class
 
@@ -152,10 +153,10 @@ module Scenarios
         end
       else
         # Завершаем скрипт и выводим сообщение в логе, что выбран то тот проект, для этого скрипта
-        puts "Необходимо использовать данный скрипт для проверки только проектов Android/IOS! Текущий тип проекта #{issue.key[0..2].truncate("-", separator: '')}"
+        puts "Необходимо использовать данный скрипт для проверки только проектов Android/IOS! Текущий тип проекта #{issue_number[0..2].truncate("-", separator: '')}"
         issue.post_comment <<-BODY
             {panel:title=Release notify!|borderStyle=dashed|borderColor=#ccc|titleBGColor=#F7D6C1|bgColor=#ff6e6e}
-              Необходимо использовать данный скрипт для проверки только проектов Android/IOS! Текущий тип проекта #{issue.key[0..2]}
+              Необходимо использовать данный скрипт для проверки только проектов Android/IOS! Текущий тип проекта #{issue_number[0..2]}
             {panel}
         BODY
         exit 0
