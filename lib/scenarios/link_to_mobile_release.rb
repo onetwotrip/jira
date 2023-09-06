88b29ce47f9c897fbd8daa4d9fd28204ab278337
Viktor Usachev <viktor.usachev@onetwotrip.com>
2023-09-06 14:11:33 +0300

    added message in script

--- lib/scenarios/link_to_mobile_release.rb
+++ lib/scenarios/link_to_mobile_release.rb
@@ -90,6 +90,7 @@ module Scenarios
             end
           end
           # Начинаем проверку существующих релизов для apps из задачи и создаем релизы
+          puts "Начинаем проверку существующих релизов для apps из задачи и создаем релизы"
           puts issues_apps_type
           puts "##############"
           issues_apps_type_uniq = issues_apps_type.select { |i| issues_apps_type.count(i) <= 2 }.uniq
