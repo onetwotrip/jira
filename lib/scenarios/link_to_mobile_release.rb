f34e5689022a2d7ba0fa1167361b1217542b6d8e
Viktor Usachev <viktor.usachev@onetwotrip.com>
2023-09-06 14:53:50 +0300

    added message in script

--- lib/scenarios/link_to_mobile_release.rb
+++ lib/scenarios/link_to_mobile_release.rb
@@ -12,7 +12,10 @@ module Scenarios
 
     def create_release_issue(project, issue, project_key = 'OTT', release_name = 'Release')
       project = project.find(project_key)
+      puts project
       release = issue.build
+      puts release
+      puts project.id
       release.save(fields: { summary: release_name, project: { id: project.id },
                              issuetype: { name: 'Release' } })
       release.fetch
